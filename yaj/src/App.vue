235c2e47c936deba463cd26ffe9bdd5bcc81f2ba
Sushantshrestha1111 <shresthasushant11@gmail.com>
2024-11-25 21:00:39 +0545

    initial commit

---
+++ yaj/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col ">
+    <div class="flex flex-col justify-center">dsfasd</div>
+    <div class="">
+    asdf
+   
+    <YellowBanner/>
+    <HelloSarkar/>
+  </div>
+
+  </div>
+</template>
+<script setup>
+import HelloSarkar from './components/form/HelloSarkar.vue'
+
+import YellowBanner from './components/other/YelloeBaner.vue'
+
+
+
+</script>
+<style>
+  
+</style>
---
+++ yaj/src/components/other/YelloeBaner.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bg-yellow-100 flex flex-row items-center rounded-md mr-5 ml-5 h-[40px]">
+        <div class="bg-yellow-500 rounded-md h-full w-[9px]"></div>
+        <div class="bg-yellow-500 rounded-full h-5 w-5 ml-5 mr-5 items-center justify-center flex">!</div>
+        <strong class="text-yellow-400">Here goes the warning message of the post </strong>
+        
+        
+    </div>
+</template>
+<script setup>
+
+</script>
+<style>
+    
+</style>
