44f83b047b5f380da26d56256c3d1ae694c047e0
Sushantshrestha1111 <shresthasushant11@gmail.com>
2024-11-26 07:10:42 +0545

    setup

---
+++ yajForm/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+ <div class="flex flex-col ">
+  <div class="bg-red-500">hello all</div>
+  <div class="mt-12">
+    <RouterView/>
+
+  </div>
+ 
+
+ </div>
+ 
+
+
+
+
+
+
+</template>
+
+<style scoped>
+.app-container {
+  background-color: #f5f5f5; /* Set the background color to white */
+  min-height: 100vh;      /* Ensure it covers the full viewport height */
+}
+</style>
---
+++ yajForm/src/components/ui/Form.vue
@@ -0,0 +1,11 @@
+<template >
+    <div>
+        this is form
+    </div>
+</template>
+<script>
+
+</script>
+<style >
+    
+</style>
