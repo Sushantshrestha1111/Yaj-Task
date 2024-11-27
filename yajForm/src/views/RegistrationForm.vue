9259e8702780e8d22b0b5a2da487fc5a30fa8e5b
Sushantshrestha1111 <shresthasushant11@gmail.com>
2024-11-27 15:34:11 +0545

    fixed gap

--- yajForm/src/views/RegistrationForm.vue
+++ yajForm/src/views/RegistrationForm.vue
@@ -40,7 +40,7 @@
 
         <div class="flex flex-col l-0 mb-4 mt-4 r-0 w-full">
          <div class="flex">
-          <span class="block mb-2  text-[#777777] font-medium"
+          <span class="block mb-1  text-[#777777] font-medium"
             >Nature of Problem  </span
           ><span class="text-red-500 ml-1">*</span>
          </div>
@@ -69,7 +69,7 @@
         </div>
         <!-- Name of Office -->
         <div class="mb-4">
-          <span class="text-[#777777] mb-2">Name of Office</span>
+          <span class="text-[#777777] mb-1">Name of Office</span>
          
           <input
           id="OfficeName"
@@ -81,69 +81,80 @@
         </div>
 
         <!--  office Addresss-->
-        <span class="text-[#777777] font-normal">Office Address:</span>
+        <span class="text-[#777777] mb-[8px] font-normal">Office Address:</span>
         <div class="flex flex-col md:flex-row mb-4 w-full gap-3">
-<!-- State -->
-<div class="flex flex-col flex-1 gap-x-4">
-  <span class="text-gray-400">State</span>
-  <div class="relative w-full">
-  <select
-    id="options"
-    v-model="selectedOption"
-    class="block w-full px-3 h-[46px] text-[#A0A0A0] pr-10 py-2 border border-[#E9E9E9] bg-white rounded-md appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-  >
-    <option class="text-black" disabled value="">Select State</option>
-    <option v-for="option in options" :key="option.value" :value="option.value">
-      {{ option.label }}
-    </option>
-  </select>
-  <!-- Custom arrow -->
-  <span class="absolute inset-y-0 right-3 flex text-[#444444] items-center pointer-events-none">
-    <img :src="down" alt="">
-  </span>
+  <!-- State -->
+  <div class="flex flex-col flex-1 gap-x-4">
+    <span class="text-gray-400">State</span>
+    <div class="relative w-full">
+      <select
+        id="options"
+        v-model="selectState"
+        class="block w-full px-3 h-[46px] text-[#A0A0A0] pr-10 py-2 border border-[#E9E9E9] bg-white rounded-md appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+      >
+        <option class="text-black" disabled value="">Select State</option>
+        <option v-for="option in states" :key="option.value" :value="option.value">
+          {{ option.label }}
+        </option>
+      </select>
+      <!-- Custom arrow -->
+      <span class="absolute inset-y-0 right-3 flex text-[#444444] items-center pointer-events-none">
+        <img :src="down" alt="">
+      </span>
+    </div>
+  </div>
+
+  <!-- District -->
+  <div class="flex flex-col flex-1 gap-x-4">
+    <span class="text-gray-400">District</span>
+    <div class="relative w-full">
+      <select
+        id="district"
+        v-model="selectDistrict"
+        :disabled="!selectState"
+        :class="{
+          'bg-gray-200': !selectState,
+          'block w-full px-3 h-[46px] text-[#A0A0A0] pr-10 py-2 border border-[#E9E9E9] bg-white rounded-md appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm': true
+        }"
+      >
+        <option class="text-black" disabled value="">Select District</option>
+        <option v-for="option in district" :key="option.value" :value="option.value">
+          {{ option.label }}
+        </option>
+      </select>
+      <!-- Custom arrow -->
+      <span class="absolute inset-y-0 right-3 flex text-[#444444] items-center pointer-events-none">
+        <img :src="down" alt="">
+      </span>
+    </div>
+  </div>
+
+  <!-- Municipality -->
+  <div class="flex flex-col flex-1 gap-x-4">
+    <span class="text-gray-400">Select Municipality</span>
+    <div class="relative w-full">
+      <select
+        id="municipality"
+        v-model="selectedOption"
+        :disabled="!selectDistrict"
+        :class="{
+          'bg-gray-200': !selectDistrict,
+          'block w-full px-3 h-[46px] text-[#A0A0A0] pr-10 py-2 border border-[#E9E9E9] bg-white rounded-md appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm': true
+        }"
+      >
+        <option class="text-black" disabled value="">Select Municipality</option>
+        <option v-for="option in options" :key="option.value" :value="option.value">
+          {{ option.label }}
+        </option>
+      </select>
+      <!-- Custom arrow -->
+      <span class="absolute inset-y-0 right-3 flex text-[#444444] items-center pointer-events-none">
+        <img :src="down" alt="">
+      </span>
+    </div>
+  </div>
 </div>
-</div>
-<!-- District -->
-<div class="flex flex-col flex-1 gap-x-4">
-  <span class="text-gray-400">District</span>
-  <div class="relative w-full">
-  <select
-    id="options"
-    v-model="selectedOption"
-    class="block w-full px-3 h-[46px] text-[#A0A0A0] pr-10 py-2 border border-[#E9E9E9] bg-white rounded-md appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-  >
-    <option class="text-black" disabled value="">Select District</option>
-    <option v-for="option in options" :key="option.value" :value="option.value">
-      {{ option.label }}
-    </option>
-  </select>
-  <!-- Custom arrow -->
-  <span class="absolute inset-y-0 right-3 flex text-[#444444] items-center pointer-events-none">
-    <img :src="down" alt="">
-  </span>
-</div>
-</div>
-<!-- Municipality -->
-<div class="flex flex-col flex-1 gap-x-4">
-  <span class="text-gray-400">Select Municipality</span>
-  <div class="relative w-full">
-  <select
-    id="options"
-    v-model="selectedOption"
-    class="block w-full px-3 h-[46px] text-[#A0A0A0] pr-10 py-2 border border-[#E9E9E9] bg-white rounded-md appearance-none focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-  >
-    <option class="text-black" disabled value="">Select a Nature of Problem</option>
-    <option v-for="option in options" :key="option.value" :value="option.value">
-      {{ option.label }}
-    </option>
-  </select>
-  <!-- Custom arrow -->
-  <span class="absolute inset-y-0 right-3 flex text-[#444444] items-center pointer-events-none">
-    <img :src="down" alt="">
-  </span>
-</div>
-</div>
-</div>
+
 
 
         <!-- Ward No -->
@@ -209,42 +220,42 @@
         </div>
         <!-- Complaint Servicity -->
         <div class="mb-4 justify-between">
-<Label class="mb-4" for="ComplaintServivity">Complaint Servicity <span class="text-red-500">*</span></Label>
+<Label class="mb-[12px]" for="ComplaintServivity">Complaint Servicity <span class="text-red-500">*</span></Label>
 
 
 <div>
   
-  <div class="flex flex-row mt-4 gap-x-6 w-full">
+  <div class="flex flex-row mb-[24px] gap-x-6 w-full">
     <!-- Common -->
-    <div class="flex flex-row items-center space-x-2 flex-1">
+    <div class="flex flex-row items-center  flex-1">
       <input
         type="radio"
         id="r1"
         value="default"
         v-model="selectedPriority"
-        class="h-4 w-4 text-blue-600 border-gray-300 focus:ring-blue-500"
+        class="h-4 w-4 text-blue-600 mr-2 border-gray-300 focus:ring-blue-500"
       />
       <label for="r1" class="font-[16px] text-[#444444]">Common</label>
     </div>
     <!-- Important -->
-    <div class="flex flex-row items-center space-x-2 flex-1">
+    <div class="flex flex-row items-center  flex-1">
       <input
         type="radio"
         id="r2"
         value="comfortable"
         v-model="selectedPriority"
-        class="h-4 w-4 text-blue-600 border-gray-300 focus:ring-blue-500"
+        class="h-4 w-4 mr-2 text-blue-600 border-gray-300 focus:ring-blue-500"
       />
       <label for="r2" class="font-[16px] text-[#444444]">Important</label>
     </div>
     <!-- Very Important -->
-    <div class="flex flex-row items-center space-x-2 flex-1">
+    <div class="flex flex-row items-center  flex-1">
       <input
         type="radio"
         id="r3"
         value="compact"
         v-model="selectedPriority"
-        class="h-4 w-4 text-blue-600 border-gray-300 focus:ring-blue-500"
+        class="h-4 w-4 text-blue-600 mr-2 border-gray-300 focus:ring-blue-500"
       />
       <label for="r3" class="font-[16px] text-[#444444] ">Very Important</label>
     </div>
@@ -305,6 +316,7 @@ const selectedPriority = ref("comfortable"); // Default value for checkbox
 // options ofr select
 const selectedOption = ref("");
 const selectState =ref("");
+const selectDistrict =ref("");
 const states= ref([
   { label: "state 1", value: "option1" },
   { label: "state2", value: "option2" },
@@ -314,6 +326,12 @@ const options = ref([
   { label: "Option 1", value: "option1" },
   { label: "Option 2", value: "option2" },
   { label: "Option 3", value: "option3" },
+]);
+
+const district = ref([
+  { label: "Option 1", value: "dis1" },
+  { label: "Option 2", value: "dis2" },
+  { label: "Option 3", value: "dis3" },
 ]);
 import down from '../assets/images/down-arrow.svg'
 </script>
